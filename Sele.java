8760df47ae8dec03d6baaaf5d37caddb6de70dab
Jyoti <jyoti@infostride.com>
2022-01-18 13:17:23 +0530

    Day 1

--- Sele.java
+++ Sele.java
@@ -27,26 +27,26 @@ public class Sele {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
-        //driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
-        //driver.findElement(By.id("shopping_cart_container")).click();
-        //driver.findElement(By.id("checkout")).click();
-        //driver.findElement(By.id("first-name")).sendKeys("Gun");
-        //driver.findElement(By.id("last-name")).sendKeys("Sharma");
-        //driver.findElement(By.id("postal-code")).sendKeys("A1A 1A1");
-        //driver.findElement(By.id("continue")).click();
-        //driver.findElement(By.id("finish")).click();
-       // driver.findElement(By.id("back-to-products")).click();
-
-        driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         driver.findElement(By.id("shopping_cart_container")).click();
-        //driver.findElement(By.id("continue-shopping")).click();
         driver.findElement(By.id("checkout")).click();
-        driver.findElement(By.id("first-name")).sendKeys("Nitin");
-        driver.findElement(By.id("last-name")).sendKeys("Kumar");
-        driver.findElement(By.id("postal-code")).sendKeys("49715");
+        driver.findElement(By.id("first-name")).sendKeys("Gun");
+        driver.findElement(By.id("last-name")).sendKeys("Sharma");
+        driver.findElement(By.id("postal-code")).sendKeys("A1A 1A1");
         driver.findElement(By.id("continue")).click();
-        driver.findElement(By.id("cancel")).click();
         driver.findElement(By.id("finish")).click();
+        driver.findElement(By.id("back-to-products")).click();
+
+        //driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
+        //driver.findElement(By.id("shopping_cart_container")).click();
+        //driver.findElement(By.id("continue-shopping")).click();
+        //driver.findElement(By.id("checkout")).click();
+        //driver.findElement(By.id("first-name")).sendKeys("Nitin");
+        //driver.findElement(By.id("last-name")).sendKeys("Kumar");
+        //driver.findElement(By.id("postal-code")).sendKeys("49715");
+        //driver.findElement(By.id("continue")).click();
+        //driver.findElement(By.id("cancel")).click();
+        //driver.findElement(By.id("finish")).click();
         //Thread.sleep(6000);
       //  driver.quit();
     }
