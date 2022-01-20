7f925c0ca36bb48bd96ee9e7c182cc10d13c0754
Jyoti <jyoti@infostride.com>
2022-01-20 19:20:55 +0530

    Day 1

---
+++ com/Swag/Main.java
@@ -0,0 +1,14 @@
+//package com.Swag;
+
+//public class Main extends Base {
+  //  public static void main(String[] args) {
+    //    Base bs=new Base();
+      //  bs.login();
+        //bs.addtocartsaucelabsbackpack();
+        //bs.addtocartsaucelabsbikelight();
+       // bs.addtocartsaucelabsbolttshirt();
+        //bs.addtocartsaucelabsfleecejacket();
+      //bs.addtocartsaucelabsonesie();
+     //   bs.AddToCart();
+    //}
+//}
