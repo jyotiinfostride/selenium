ece5027d4558ca3cea41de46e95b585a991ccc84
Jyoti <jyoti@infostride.com>
2022-01-27 16:17:28 +0530

    commit1

--- src/demo/test/Assignment.java
+++ src/demo/test/Assignment.java
@@ -11,8 +11,8 @@ public class Assignment {
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://www.facebook.com/");
 		driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
-		driver.findElement(By.xpath("//button[@class='_6j mvm _6wk _6wl _58mi _3ma _6o _6v']//child::button")).click();	
-		driver.findElement(By.xpath("//input[@name='firstname']//parent::div")).sendKeys("Jyoti");
+		//driver.findElement(By.xpath("//button[@class='_6j mvm _6wk _6wl _58mi _3ma _6o _6v']//child::button")).click();	
+		driver.findElement(By.xpath("//button[@class='_6j mvm _6wk _6wl _58mi _3ma _6o _6v']//parent::div//preceding-sibling::div//child::input[@name='firstname']"));
 		
 	}
 		
